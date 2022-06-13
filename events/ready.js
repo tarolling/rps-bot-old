cd0184326e83c897b2607da6dcce7a7885050c13
rhit-joensbj <34352544+tarolling@users.noreply.github.com>
2022-06-12 21:22:51 -0500

    Broken code but just saving changes

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,6 @@ module.exports = {
     once: true,
     execute(client) {
         console.log(`Logged in as ${client.user.tag}`);
-        setup(client);
+        setup();
     }
 };
--- setup.js
+++ setup.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
 const { MODE_ARG } = require('./config/settings.json');
+const ranks = require('./config/ranks.json');
 
 module.exports = () => {
+    // Initializing slash commands
     const token = (!process.argv[MODE_ARG]) ? process.env.PROD_TOKEN : process.env.DEV_TOKEN;
     const clientId = (!process.argv[MODE_ARG]) ? process.env.PROD_CLIENT_ID : process.env.DEV_CLIENT_ID;
     const guildId = (!process.argv[MODE_ARG]) ? process.env.PROD_GUILD_ID : process.env.DEV_GUILD_ID;
@@ -25,4 +27,10 @@ module.exports = () => {
     rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
         .then(() => console.log('Successfully registered application commands.'))
         .catch(console.error);
+
+    // Initializing rank queues
+    for (const rank in ranks) {
+        console.log(rank);
+    }
+    console.log(process);
 };
