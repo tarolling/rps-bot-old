9b83bccc8fb1d8589065a876e4461cf7cf5b68a9
tarolling <benitojoens@gmail.com>
2024-07-08 01:21:39 -0400

    Cleaning up code and deps

--- src/clubs/db/join.js
+++ src/clubs/db/join.js
@@ -1,10 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
-// eslint-disable-next-line
-const uri = process.env.DB_URI;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 module.exports = async (interaction) => {
-    const dbClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    const dbClient = new MongoClient(process.env.DB_URI, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
     const userId = interaction.user.id;
     const playerQuery = { members: userId };
 
