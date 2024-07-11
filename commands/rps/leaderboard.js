e2e1ac78c0902fe92c11d3bffccc0267359e41ed
tarolling <benitojoens@gmail.com>
2024-07-10 22:26:53 -0400

    Added leaderboard fix

--- commands/rps/leaderboard.js
+++ commands/rps/leaderboard.js
@@ -1,18 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
-const updateLeaderboards = require('../../src/db/updateLeaderboards');
+const { fetchLeaderboards } = require('../../src/db');
+const { leaderboard } = require('../../src/utils/embeds');
 
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('lb')
-        .setDescription('View the global leaderboards.'),
+        .setDescription('View the global leaderboard.'),
     async execute(interaction) {
         try {
-            await interaction.deferReply({ ephemeral: true });
-            await updateLeaderboards(interaction);
+            await interaction.deferReply();
+            const players = await fetchLeaderboards();
+            if (!players) {
+                return interaction.editReply({ content: 'Huh. I guess there are no active players.', ephemeral: true });
+            }
+            return interaction.editReply({ embeds: [leaderboard(players)] });
         } catch (err) {
             console.error(err);
-            return interaction.editReply({ content: 'An error occurred while trying to refresh the leaderboards.', ephemeral: true });
+            return interaction.editReply({ content: 'An error occurred while trying to fetch the leaderboards.', ephemeral: true });
         }
     }
 };
--- src/db/index.js
+++ src/db/index.js
@@ -1,22 +1,22 @@
 const adjustElo = require('./adjustElo');
 const adjustStats = require('./adjustStats');
 const deletePlayer = require('./deletePlayer');
+const fetchLeaderboards = require('./fetchLeaderboards');
 const findPlayer = require('./findPlayer');
 const rankValidate = require('./rankValidate');
 const registerPlayer = require('./registerPlayer');
 const resetPlayer = require('./resetPlayer');
 const setPlayerStats = require('./setPlayerStats');
-const updateLeaderboards = require('./updateLeaderboards');
 
 
 module.exports = {
     adjustElo,
     adjustStats,
     deletePlayer,
+    fetchLeaderboards,
     findPlayer,
     rankValidate,
     registerPlayer,
     resetPlayer,
-    setPlayerStats,
-    updateLeaderboards
+    setPlayerStats
 };
