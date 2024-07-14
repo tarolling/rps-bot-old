49017f782298548353bcde843f9bbd6c088c768d
tarolling <benitojoens@gmail.com>
2024-07-14 13:50:09 -0400

    Remove file refs

--- src/db/index.js
+++ src/db/index.js
@@ -1,22 +1,16 @@
 const adjustElo = require('./adjustElo');
 const adjustStats = require('./adjustStats');
-const deletePlayer = require('./deletePlayer');
 const fetchLeaderboards = require('./fetchLeaderboards');
 const findPlayer = require('./findPlayer');
 const rankValidate = require('./rankValidate');
 const registerPlayer = require('./registerPlayer');
-const resetPlayer = require('./resetPlayer');
-const setPlayerStats = require('./setPlayerStats');
 
 
 module.exports = {
     adjustElo,
     adjustStats,
-    deletePlayer,
     fetchLeaderboards,
     findPlayer,
     rankValidate,
     registerPlayer,
-    resetPlayer,
-    setPlayerStats
 };
