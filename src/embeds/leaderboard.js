15863b17e7ae88e546d8786fe34e16aae18642cd
tarolling <benitojoens@gmail.com>
2024-07-14 17:34:01 -0400

    Add tag to leaderboard

--- src/embeds/leaderboard.js
+++ src/embeds/leaderboard.js
@@ -5,6 +5,9 @@ module.exports = (data) => {
     return new EmbedBuilder()
         .setColor(defaultColor)
         .setTitle('Global Leaderboard')
-        .addFields(data.map(({ player, elo }) => ({ name: `${elo}`, value: `${player.username}` })))
+        .addFields(data.map(({ player, elo }) => ({
+            name: `${elo}`,
+            value: `${player.username} (<@${player.id}>)`
+        })))
         .setFooter(footer);
 };
