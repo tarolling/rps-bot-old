31dd041c24583e8d59e4172548e5cbf0f359b692
tarolling <benitojoens@gmail.com>
2024-07-28 15:40:19 -0400

    feat: post stats to top.gg

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const { ShardingManager } = require('discord.js');
+const { AutoPoster } = require('topgg-autoposter');
 
-const token = (process.argv.findIndex(s => s === 'dev') == -1) ? process.env.PROD_TOKEN : process.env.DEV_TOKEN;
+const token = (process.argv.findIndex(s => s === 'dev') === -1) ? process.env.PROD_TOKEN : process.env.DEV_TOKEN;
 
 let args = []
-if (process.argv.findIndex(s => s === 'dev') != -1) args.push('dev')
+if (process.argv.findIndex(s => s === 'dev') !== -1) args.push('dev')
 
 const manager = new ShardingManager('./bot.js', { execArgv: ['-r', 'dotenv/config'], shardArgs: args, token: token });
 
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+/* Top.gg stats */
+if (process.argv.findIndex(s => s === 'dev') === -1) {
+    const ap = AutoPoster(process.env.TOPGG_API_TOKEN, manager);
+    ap.on('posted', (stats) => {
+        console.log(`Posted stats to Top.gg | ${stats.serverCount} servers`);
+    });
+    ap.on('error', (error) => {
+        console.warn(`Unable to post stats to Top.gg | ${error}`);
+    });
+}
 
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
 manager.spawn();
